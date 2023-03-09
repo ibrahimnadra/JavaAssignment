a73ea72a1b302dbd7fed55f33ed989b92f72a20a
ibrahimnadra <124992299+ibrahimnadra@users.noreply.github.com>
2023-03-09 18:31:28 +0530

    Update Main.java

--- src/main/java/com/knoldus/errorHandling/task2/Main.java
+++ src/main/java/com/knoldus/errorHandling/task2/Main.java
@@ -1,13 +1,3 @@
-//Create a Java program that generates a random number between 1 and 100.
-//The program should ask the user to enter a number.
-//If the number entered by the user is less than the random number, the program should display
-// the message "Too Low" and ask the user to enter another number.
-//If the number entered by the user is greater than the random number, the program should display the
-// message "Too High" and ask the user to enter another number.
-//If the user enters the correct number, the program should display the message "You guessed it!"
-// and terminate.
-//Use the try-catch block to handle the exception in case the user enters an invalid input
-// (e.g. a string instead of a number). The program should display the error message "Invalid input. Please enter a number." and ask the user to enter another number.
 package com.knoldus.errorHandling.task2;
 
 import java.util.Random;
