27fb70c2b5673b03315b621361c0f0edf04b9252
ibrahimnadra <nadra.ibrahim@knoldus.com>
2023-03-13 02:16:52 +0530

    "Sixth Java Assignment"

--- src/main/java/com/knoldus/task5/SalesTeamPerformanceImplementation.java
+++ src/main/java/com/knoldus/task5/SalesTeamPerformanceImplementation.java
@@ -4,24 +4,69 @@
 // How would you use Java aggregations to calculate the total sales made by each salesperson?
 package com.knoldus.task5;
 
+import java.util.Scanner;
+
 class Sales{
-    private double amount;
-    //days of a month in which the salesperson made sale
-    private double durationInDays;
+    private double salesPerDay;
+    //Number of days he made sale
+    private double saleDuration;
+
+    public void setSaleDuration(double saleDuration) {
+        this.saleDuration = saleDuration;
+    }
+
+    public void setSalesPerDay(double salesPerDay) {
+        this.salesPerDay = salesPerDay;
+    }
+
+    public double getSaleDuration() {
+        return saleDuration;
+    }
+
+    public double getSalesPerDay() {
+        return salesPerDay;
+    }
 }
 class SalesPerson{
     private String name;
-    private Sales salesIn;
-    private Sales salesInApril;
-    private Sales salesInMay;
-    public void getTotalSales(){
-
+    private Sales sales = new Sales();
+    SalesPerson(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Name : ");
+        name = scanner.next();
+        System.out.println("Number of days of sale : ");
+        sales.setSaleDuration(scanner.nextDouble());
+        System.out.println("Sales per day : ");
+        sales.setSalesPerDay(scanner.nextDouble());
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Sales getSales() {
+        return sales;
+    }
+
+    public double getTotalSales(){
+        return (sales.getSaleDuration() * sales.getSalesPerDay());
+    }
+
+    public void setSales(Sales sales) {
+        this.sales = sales;
+    }
 }
 
 
 
 public class SalesTeamPerformanceImplementation {
+    public static void main(String[] args) {
+        SalesPerson salesPerson = new SalesPerson();
+        System.out.println("Total Sales made by " +  salesPerson.getName() + " are " + salesPerson.getTotalSales());
+    }
 
 }
