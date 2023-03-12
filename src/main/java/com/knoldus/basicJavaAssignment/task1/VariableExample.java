0509f40d4fe6d6a99a7ead41d521c487181cf9a5
ibrahimnadra <nadra.ibrahim@knoldus.com>
2023-03-12 23:47:52 +0530

    "Sixth Java Assignment"

--- src/main/java/com/knoldus/basicJavaAssignment/task1/VariableExample.java
+++
@@ -1,9 +0,0 @@
-//Write a program to declare and initialize an integer variable named "age" with the value of 25.
-package com.knoldus.basicJavaAssignment.task1;
-
-public class VariableExample {
-    public static void main(String[] args) {
-        int age = 25;
-        System.out.println("Age = " + age);
-    }
-}
